2096b05b18bdd75c8430ea227228f34f3a6adc3c
DELL <DELL@DESKTOP-6VHHS07>
2025-10-07 20:50:24 +0530

    i finished assignment

--- corejava/src/day4/Thirdpackage/PrivateConstructor.java
+++ corejava/src/day4/Thirdpackage/PrivateConstructor.java
@@ -1,5 +1,18 @@
 package day4.Thirdpackage;
 
-public class PrivateConstructor {
 
+public class PrivateConstructorDemo {
+	public static void main(String s[]) {
+		// Can't create object with private constructor
+		// MyClass m1=new MyClass();
+		
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
+	}
 }
